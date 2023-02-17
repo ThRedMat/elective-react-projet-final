c437ca1b685147de6b8c29b8ccea65b7a87174b4
b.bonnin <b.bonnin@efysoft.fr>
2023-02-17 11:33:16 +0100

    Done Router sur le titre navbar
    Partie terminée

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">MangasApp</span>
+        <Link to="/"><span className="navbar-brand mb-0 h1">MangasApp</span></Link>
       </div>
     </nav>
   );
