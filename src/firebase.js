6d8bf8df83de2aeb3465a94fa07abeee47912fd4
b.bonnin <b.bonnin@efysoft.fr>
2023-02-22 16:48:28 +0100

    ✅ Done setup firebase 🚧page de login et de signup
    Partie terminée

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhoA_8WI2RP36KYVbBhR_6xd_sWylKuyA",
+  authDomain: "mangalist-93c77.firebaseapp.com",
+  projectId: "mangalist-93c77",
+  storageBucket: "mangalist-93c77.appspot.com",
+  messagingSenderId: "359155840001",
+  appId: "1:359155840001:web:2b6b9d190d0b5e208a7c00",
+  measurementId: "G-29BC3PVQRT"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore();
+
+export {auth, db};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,131 +1,90 @@
-import React, { useState } from 'react';
-import "../styles/Login.css";
+import React, {useState} from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Login = () => {
-  const [userInfos, setUserInfos] = useState({
-    email: '',
-    password: '',
-    lastname: '',
-    firstname: ''
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLogged, setIsLogged] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      setIsLogged(true);
-    }, 2000);
-  };
-
-  const handleChange = e => {
-    setUserInfos({ ...userInfos, [e.target.name]: e.target.value });
-  };
-
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  const handleSave = e => {
-    e.preventDefault();
-    setIsEditing(false);
-  };
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-
-  if (isLogged && !isEditing) {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
-        <div className='bg-white rounded-lg shadow-lg p-10'>
-          <h1 className='text-2xl mb-6'>Your Profile :</h1>
-          <p className='mb-2'>Email : {userInfos.email}</p>
-          <p className='mb-2'>Last Name : {userInfos.lastname}</p>
-          <p className='mb-6'>First Name : {userInfos.firstname}</p>
-          <div className='flex justify-between'>
-            <button onClick={handleEdit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Edit
-            </button>
-            <button onClick={() => setIsLogged(false)} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  } else if (isLogged && isEditing) {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
-        <div className='bg-white rounded-lg shadow-lg p-10'>
-          <form onSubmit={handleSave}>
-            <input
-              type="email"
-              name="email"
-              value={userInfos.email}
-              onChange={handleChange}
-              placeholder="Email"
-              className="bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 mb-4 px-4 py-2 rounded w-full"
-            />
-            <input
-              type="text"
-              name="lastname"
-              value={userInfos.lastname}
-              onChange={handleChange}
-              placeholder="Last Name"
-              className="bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 mb-4 px-4 py-2 rounded w-full"
-            />
-            <input
-              type="text"
-              name="firstname"
-              value={userInfos.firstname}
-              onChange={handleChange}
-              placeholder="First Name"
-              className="bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 mb-6 px-4 py-2 rounded w-full"
-            />
-            <div className='flex justify-between'>
-              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Save
-                </button>
-                <button onClick={() => setIsLogged(false)} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                    Logout
-                </button>
-            </div>
-            </form>
-        </div>
-        </div>
-    );
-    } else {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
-        <div className='bg-white rounded-lg shadow-lg p-10'>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              value={userInfos.email}
-              onChange={handleChange}
-              placeholder="Email"
-              className="bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 mb-4 px-4 py-2 rounded w-full"
-            />
-            <input
-              type="password"
-              name="password"
-              value={userInfos.password}
-              onChange={handleChange}
-              placeholder="Password"
-              className="bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 mb-4 px-4 py-2 rounded w-full"
-            />
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
-    );
+    const navigate = useNavigate();
+ 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+ 
+    const onSubmit = async (e) => {
+      e.preventDefault()
+     
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/login")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+        });
+ 
+   
     }
-};
+ 
+  return (
+    <main >        
+        <section>
+            <div>
+                <div>                  
+                    <h1> FocusApp </h1>                                                                            
+                    <form>                                                                                            
+                        <div>
+                            <label htmlFor="email-address">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                label="Email address"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}  
+                                required                                    
+                                placeholder="Email address"                                
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                label="Create password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required                                 
+                                placeholder="Password"              
+                            />
+                        </div>                                             
+                        
+                        <button
+                            type="submit" 
+                            onClick={onSubmit}                        
+                        >  
+                            Sign up                                
+                        </button>
+                                                                     
+                    </form>
+                   
+                    <p>
+                        Already have an account?{' '}
+                        <NavLink to="/login" >
+                            Sign in
+                        </NavLink>
+                    </p>                   
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
+ 
 export default Login;
