f9a1d83bfb97714c042da240dcfcd5bf0701225d
mAthieu33290 <55395305+mAthieu33290@users.noreply.github.com>
2023-03-07 14:16:03 +0100

    footer ok

--- src/pages/InfoManga.jsx
+++ src/pages/InfoManga.jsx
@@ -1,27 +1,29 @@
 import '../App.css';
 import Navbar from "../components/Navbar";
 import MangaInfos from "../components/MangaInfos";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "../states/store";
 import React from 'react';
+import Footer from '../components/Footer';
 
 const InfoManga = () => {
 
     /* const { mangaId } = useParams();
     console.log(mangaId) */
 
-    
+
 
 
     /* const { userId } = useParams();
     const { Manga } = fetchMangas */
     return (
         <div>
-        <Provider store={store}>
-            <Navbar />
-            <MangaInfos />
+            <Provider store={store}>
+                <Navbar />
+                <MangaInfos />
 
-        </Provider>
+            </Provider>
+            <Footer />
         </div>
     );
 }
