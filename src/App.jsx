23959c4b897ac48db2971b4a3f92a01c63fa6495
b.bonnin <b.bonnin@efysoft.fr>
2023-03-10 15:58:49 +0100

    ✅ clear code et ajout de style
    Partie terminée

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,21 @@ import './App.css';
 import Index from "./pages/Index";
 import InfoManga from "./pages/InfoManga";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AnimeList from './components/AnimeList';
 import NotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import ProfilInfos from './pages/ProfilInfos';
-import Suggestions from './components/Suggestions';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/anime" element={<AnimeList />} />
           <Route path="/manga/:mangaId" element={<InfoManga />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Profil" element={<ProfilInfos />} />
-          <Route path="/suggestions" element={<Suggestions />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/MangaInfos.jsx
+++ src/components/MangaInfos.jsx
@@ -29,34 +29,56 @@ const MangaInfos = () => {
     );
   }
 
+  const selectedManga = mangas.find((manga) => manga.mal_id === parseInt(mangaId));
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center space-y-8">
-      <h1 className="text-3xl font-bold">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).title}</h1>
-      <img className="w-64 h-96 object-cover rounded-md shadow-lg" src={IfImageNotFound(mangas, mangaId)} alt="image" />
+      <h1 className="text-3xl font-bold">{selectedManga.title}</h1>
+      <img
+        className="w-64 h-96 object-cover rounded-md shadow-lg transform hover:scale-110 transition duration-500 ease-in-out"
+        src={IfImageNotFound(mangas, mangaId)}
+        alt="image"
+      />
       <div className="grid grid-cols-2 gap-4 w-full max-w-3xl">
         <div className="flex flex-col space-y-2">
           <div className="bg-gray-100 px-4 py-2 rounded-lg">
-            <p className="text-gray-500 font-semibold">Nombre de volumes publiés</p>
-            <p className="text-lg">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).volumes}</p>
+            <p className="text-gray-500 font-semibold">
+              Nombre de volumes publiés
+            </p>
+            <p className="text-lg">{selectedManga.volumes}</p>
           </div>
           <div className="bg-gray-100 px-4 py-2 rounded-lg">
-            <p className="text-gray-500 font-semibold">Nombre de chapitres publiés</p>
-            <p className="text-lg">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).chapters}</p>
+            <p className="text-gray-500 font-semibold">
+              Nombre de chapitres publiés
+            </p>
+            <p className="text-lg">{selectedManga.chapters}</p>
           </div>
         </div>
         <div className="flex flex-col space-y-2">
           <div className="bg-gray-100 px-4 py-2 rounded-lg">
             <p className="text-gray-500 font-semibold">Statut</p>
-            <p className="text-lg">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).status}</p>
+            <p className="text-lg">{selectedManga.status}</p>
           </div>
           <div className="bg-gray-100 px-4 py-2 rounded-lg">
-            <p className="text-gray-500 font-semibold">Première publication</p>
-            <p className="text-lg">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).published.string}</p>
+            <p className="text-gray-500 font-semibold">
+              Première publication
+            </p>
+            <p className="text-lg">{selectedManga.published.string}</p>
+          </div>
+          <div className="bg-gray-100 px-4 py-2 rounded-lg">
+            <p className="text-gray-500 font-semibold">Type</p>
+            <p className="text-lg">{selectedManga.type}</p>
           </div>
         </div>
       </div>
-      <p className="text-lg max-w-3xl">{mangas.find((manga) => manga.mal_id === parseInt(mangaId)).synopsis}</p>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleAddFavorite}>
+      <div className="bg-gray-100 px-4 py-2 rounded-lg w-full max-w-3xl">
+        <p className="text-gray-500 font-semibold">Synopsis</p>
+        <p className="text-lg">{selectedManga.synopsis}</p>
+      </div>
+      <button
+        className="bg-green-500 text-white px-4 py-2 rounded-lg w-full max-w-3xl"
+        onClick={handleAddFavorite}
+      >
         Ajouter aux favoris
       </button>
     </div>
@@ -73,3 +95,4 @@ const IfImageNotFound = (mangas, mangaId) => {
 }
 
 export default MangaInfos;
+
